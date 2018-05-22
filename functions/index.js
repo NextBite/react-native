f5b6d43261c01e58dd8f284ebb94aea411e1de73
Lisa Koss <lisakoss@uw.edu>
2018-05-21 23:35:07 -0700

    Fix error in cloud func

--- functions/index.js
+++ functions/index.js
@@ -6,21 +6,22 @@ admin.initializeApp(functions.config().firebase);
 
 
 // Listens for new messages added to messages/:pushId
-exports.pushNotification = functions.database.ref('/listings/{pushId}').onCreate( event => {
+exports.pushNotification = functions.database.ref('/listings/{pushId}').onCreate(event => {
   // Create a notification
-    const payload = {
-        notification: {
-            title: "New Rescue Available",
-            body: `${event.after._data.location.split(",")[0]} has a pickup available. Expires at ${String(new Date(event.after._data.expirationDate)).split(" ")[4].slice(0, -3)}.`,
-            sound: "default"
-        },
-    };
+  console.log("event data", event._data);
+  const payload = {
+    notification: {
+      title: "New Rescue Available",
+      body: `${event._data.location.split(",")[0]} has a pickup available. Expires at ${String(new Date(event._data.expirationDate)).split(" ")[4].slice(0, -3)}.`,
+      sound: "default"
+    },
+  };
 
   //Create an options object that contains the time to live for the notification and the priority
-    const options = {
-        priority: "high",
-        timeToLive: 60 * 60 * 24
-    };
+  const options = {
+    priority: "high",
+    timeToLive: 60 * 60 * 24
+  };
 
-    return admin.messaging().sendToTopic("newListing", payload, options);
+  return admin.messaging().sendToTopic("newListing", payload, options);
 });
