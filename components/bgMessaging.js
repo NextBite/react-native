39760dd5f226bf0f710427cc32e298d85fb2b481
Lisa Koss <lisakoss@uw.edu>
2018-05-25 00:00:05 -0700

    Changes to cloud func

--- components/bgMessaging.js
+++ components/bgMessaging.js
@@ -4,6 +4,6 @@ import RNfirebase from 'react-native-firebase';
 export default async (message) => {
     // handle your message
     // This handler method must return a promise and resolve within 60 seconds.
-
+    console.log("message", message);
     return Promise.resolve();
 }
